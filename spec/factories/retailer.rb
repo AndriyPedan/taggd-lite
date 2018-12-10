5407c7725c9168e0771c5f00c3a14814c6bcbf7e
AndriyPedan <andriypedan.p@gmail.com>
2018-12-10 10:47:35 +0200

    Minor fixes

--- spec/factories/retailer.rb
+++ spec/factories/retailer.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :retailer do
     name { FFaker::Name.name }
-    username { FFaker::Internet.username }
+    username { FFaker::Internet.user_name }
     business_id { FFaker::IdentificationBR.cnpj }
     token { FFaker::IdentificationBR.cnpj }
+    current { true }
   end
 end
--- spec/factories/subscription.rb
+++ spec/factories/subscription.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :subscription do
-    plan_token { Faker::Internet.password(12) }
+    plan_token { FFaker::IdentificationBR.cnpj }
     plan_type { %w(essentials complete enterprise).sample }
     active { false }
   end
